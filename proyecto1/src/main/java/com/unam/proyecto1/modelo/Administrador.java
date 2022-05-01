490c834249b413bec80757ac784d59f7491b766d
axelthepony27 <axelgarcia@ciencias.unam.mx>
2022-05-01 18:57:51 -0500

    Agrega modelo/ y Administrador.java.
    Este commit agrega el paquete modelo/, el cual contiene las clases que
    representan el modelo del proyecto. Tambien agrega las dependencias
    necesarias al pom.

---
+++ proyecto1/src/main/java/com/unam/proyecto1/modelo/Administrador.java
@@ -0,0 +1,25 @@
+package com.unam.proyecto1.modelo;
+
+import javax.persistence.*;
+import lombok.Data;
+
+@Data
+@Table(name = "Usuario")
+@Entity
+public class Administrador {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_Administrador")
+    private Integer id_Administrador;
+    @Column
+    private String nombres;
+    @Column
+    private String apellido_P;
+    @Column
+    private String apellido_M;
+    @Column
+    private String email;
+    @Column
+    private String password;
+
+}
