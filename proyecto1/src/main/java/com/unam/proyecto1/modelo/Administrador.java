f3e26acde81c11ee47ac83c02b34951fb7e18186
axelthepony27 <axelgarcia@ciencias.unam.mx>
2022-05-01 20:49:11 -0500

    Agrega getters y setters a Administrador.

--- proyecto1/src/main/java/com/unam/proyecto1/modelo/Administrador.java
+++ proyecto1/src/main/java/com/unam/proyecto1/modelo/Administrador.java
@@ -22,4 +22,51 @@ public class Administrador {
     @Column
     private String password;
 
+    public Integer getId_Administrador() {
+        return id_Administrador;
+    }
+
+    public void setId_Administrador(Integer id_Administrador) {
+        this.id_Administrador = id_Administrador;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido_P() {
+        return apellido_P;
+    }
+
+    public void setApellido_P(String apellido_P) {
+        this.apellido_P = apellido_P;
+    }
+
+    public String getApellido_M() {
+        return apellido_M;
+    }
+
+    public void setApellido_M(String apellido_M) {
+        this.apellido_M = apellido_M;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
--- proyecto1/src/main/resources/db.migrations/schema-postgres.sql
+++ proyecto1/src/main/resources/db.migrations/schema-postgres.sql
@@ -1,8 +1,9 @@
-CREATE TABLE Administrador{
-    id_Administrador INT AUTO_INCREMENT PRIMARY KEY,
-    nombre VARCHAR(50),
-    apellido_P VARCHAR(30),
-    apellido_M VARCHAR(30),
-    email VARCHAR(50),
-    password VARCHAR(50)
-};
+DROP TABLE IF EXISTS Administrador;
+CREATE TABLE Administrador (
+    id_Administrador SERIAL PRIMARY KEY,
+    nombre           VARCHAR(50),
+    apellido_P       VARCHAR(30),
+    apellido_M       VARCHAR(30),
+    email            VARCHAR(50),
+    password         VARCHAR(50)
+);
