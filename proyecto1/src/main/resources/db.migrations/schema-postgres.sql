b3e2938aed79f636a7c2bb7ab0ecfea1de123293
axelthepony27 <axelgarcia@ciencias.unam.mx>
2022-05-01 20:10:41 -0500

    Agrega los cripts de PostgreSQL para manejar la BD.
    Este commit agrega los scripts data-postgres.sql y schema-postgres.sql a
    resources/db.migrations/, los cuales crean y poblan las tablas necesarias
    en la base de datos.

---
+++ proyecto1/src/main/resources/db.migrations/schema-postgres.sql
@@ -0,0 +1,8 @@
+CREATE TABLE Administrador{
+    id_Administrador INT AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(50),
+    apellido_P VARCHAR(30),
+    apellido_M VARCHAR(30),
+    email VARCHAR(50),
+    password VARCHAR(50)
+};
