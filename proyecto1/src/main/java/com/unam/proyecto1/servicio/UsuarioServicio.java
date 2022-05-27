eca70bc9d9149ec14d2c4b6c3135ce5f555b83eb
axelthepony27 <axelgarcia@ciencias.unam.mx>
2022-05-27 18:45:46 -0500

    Modifica lso mÃ©todos pare crear los tipos de usuarios.
    Este commit modifica los metoso implementados en UsuarioServicio para
    crear cada tipo de usuario. Ahora, hay un metodo general, y 3 metodos
    especializados para cada tipo de rol creable (Entrenador, Competidor y
    Juez).

--- proyecto1/src/main/java/com/unam/proyecto1/servicio/UsuarioServicio.java
+++ proyecto1/src/main/java/com/unam/proyecto1/servicio/UsuarioServicio.java
@@ -10,9 +10,11 @@ import java.util.Set;
 
 public interface UsuarioServicio {
     Usuario creaUsuario(String email, String password, String nombre, String apellido_p, String apellido_m);
+    Usuario creaUsuarioEntrenador(String email, String password, String nombre, String apellido_p, String apellido_m);
     Usuario creaUsuarioCompetidor(String email, String password, String nombre,
                                          String apellido_p, String apellido_m, String sexo, Date fecha,
                                          int peso, int altura);
+    Usuario creaUsuarioJuez(String email, String password, String nombre, String apellido_p, String apellido_m);
     public void eliminarUsuario(Integer id);
     public Usuario actualizarUsuario(Usuario usuario);
 }
--- proyecto1/src/main/java/com/unam/proyecto1/servicio/UsuarioServicioImpl.java
+++ proyecto1/src/main/java/com/unam/proyecto1/servicio/UsuarioServicioImpl.java
@@ -9,68 +9,79 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.Date;
-import java.util.Set;
+
 @Service
 public class UsuarioServicioImpl implements UsuarioServicio {
 
     @Autowired
-    private UsuarioRepositorio usuarioRepo;
+    private UsuarioRepositorio usuarioRepositorio;
 
     @Autowired
     private RolRepositorio rolRepositorio;
 
     @Override
-    public Usuario creaUsuario(String email, String password, String nombre, String apellido_p, String apellido_m){
-        if (usuarioRepo.existsUsuarioByEmail(email)) {
+    public Usuario creaUsuario(String email, String password, String nombre, String apellido_p, String apellido_m) {
+        Usuario nuevoUsuario = new Usuario();
+        nuevoUsuario.setEmail(email);
+        nuevoUsuario.setNombre(nombre);
+        nuevoUsuario.setApellido_P(apellido_p);
+        nuevoUsuario.setApellido_M(apellido_m);
+        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        nuevoUsuario.setPassword(passwordEncoder.encode(password));
+        nuevoUsuario.setEnabled(1);
+        return nuevoUsuario;
+    }
+
+    @Override
+    public Usuario creaUsuarioEntrenador(String email, String password, String nombre, String apellido_p, String apellido_m) {
+        if (usuarioRepositorio.existsUsuarioByEmail(email)) {
             return null;
         }
         Rol rol = rolRepositorio.findByNombre("ROLE_ENTRENADOR");
-        Usuario nuevoUsuario = new Usuario();
-        nuevoUsuario.setEmail(email);
-        nuevoUsuario.setNombre(nombre);
-        nuevoUsuario.setApellido_P(apellido_p);
-        nuevoUsuario.setApellido_M(apellido_m);
-        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
-        nuevoUsuario.setPassword(passwordEncoder.encode(password));
-        nuevoUsuario.setEnabled(1);
-        nuevoUsuario.addRol(rol);
-        nuevoUsuario.setPeso(0);
-        nuevoUsuario.setAltura(0);
-        nuevoUsuario.setSexo("n/a");
-        nuevoUsuario.setFecha_nacimiento(null);
-
-
-        return usuarioRepo.save(nuevoUsuario);
+        Usuario nuevoEntrenador = creaUsuario(email, password, nombre, apellido_p, apellido_m);;
+        nuevoEntrenador.setPeso(0);
+        nuevoEntrenador.setAltura(0);
+        nuevoEntrenador.setSexo("n/a");
+        nuevoEntrenador.setFecha_nacimiento(null);
+        nuevoEntrenador.addRol(rol);
+        return usuarioRepositorio.save(nuevoEntrenador);
     }
+
     @Override
     public Usuario creaUsuarioCompetidor(String email, String password, String nombre,
-                                         String apellido_p, String apellido_m, String sexo, Date fecha,
+                                         String apellido_p, String apellido_m, String sexo, Date fecha_nac,
                                          int peso, int altura){
-        if (usuarioRepo.existsUsuarioByEmail(email)) {
+        if (usuarioRepositorio.existsUsuarioByEmail(email)) {
             return null;
         }
         Rol rol = rolRepositorio.findByNombre("ROLE_COMPETIDOR");
-        Usuario nuevoUsuario = new Usuario();
-        nuevoUsuario.setEmail(email);
-        nuevoUsuario.setNombre(nombre);
-        nuevoUsuario.setApellido_P(apellido_p);
-        nuevoUsuario.setApellido_M(apellido_m);
-        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
-        nuevoUsuario.setPassword(passwordEncoder.encode(password));
-        nuevoUsuario.setEnabled(1);
-        nuevoUsuario.addRol(rol);
-        nuevoUsuario.setPeso(peso);
-        nuevoUsuario.setAltura(altura);
-        nuevoUsuario.setSexo(sexo);
-        nuevoUsuario.setFecha_nacimiento(fecha);
-        return usuarioRepo.save(nuevoUsuario);
+        Usuario nuevoCompetidor = creaUsuario(email, password, nombre, apellido_p, apellido_m);
+        nuevoCompetidor.setPeso(peso);
+        nuevoCompetidor.setAltura(altura);
+        nuevoCompetidor.setSexo(sexo);
+        nuevoCompetidor.setFecha_nacimiento(fecha_nac);
+        nuevoCompetidor.addRol(rol);
+        return usuarioRepositorio.save(nuevoCompetidor);
     }
+
+    @Override
+    public Usuario creaUsuarioJuez(String email, String password, String nombre, String apellido_p, String apellido_m) {
+        if (usuarioRepositorio.existsUsuarioByEmail(email)) {
+            return null;
+        }
+        Rol rol = rolRepositorio.findByNombre("ROLE_JUEZ");
+        Usuario nuevoJuez = creaUsuario(email, password, nombre, apellido_p, apellido_m);
+        nuevoJuez.addRol(rol);
+        return usuarioRepositorio.save(nuevoJuez);
+    }
+
     @Override
     public void eliminarUsuario(Integer id){
-        usuarioRepo.deleteById(id);
+        usuarioRepositorio.deleteById(id);
     }
+
     @Override
     public Usuario actualizarUsuario(Usuario competidor) {
-        return usuarioRepo.save(competidor);
+        return usuarioRepositorio.save(competidor);
     }
 }
