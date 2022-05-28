34c33a146e9f975dd2021ff903f4e1cc87dc2557
Fernando <fernandondin@ciencias.unam.mx>
2022-05-28 17:10:07 -0500

    En entrenador buscar ya solo busca los usuarios que el registro  y en la paagina de inicio ya cuenta sus competidores registrados

--- proyecto1/src/main/java/com/unam/proyecto1/repositorio/UsuarioRepositorio.java
+++ proyecto1/src/main/java/com/unam/proyecto1/repositorio/UsuarioRepositorio.java
@@ -2,6 +2,8 @@ package com.unam.proyecto1.repositorio;
 
 import com.unam.proyecto1.modelo.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -11,4 +13,14 @@ public interface UsuarioRepositorio extends JpaRepository<Usuario, Integer> {
     boolean existsUsuarioByEmail(String email);
     Usuario findByEmail(String email);
     Usuario findById(int id);
+    /*Consulta todos los competidores asociados a un entrenador por el id del entrenador*/
+    @Query(nativeQuery = true, value = "select * from usuario where entrenador_Id = :id")
+    List<Usuario> findCompetidoresRegistrados(@Param("id")Integer id);
+    /**/
+    @Query(nativeQuery = true, value = "select disciplina_Id from (Competidores_Eventos natural join Evento)")
+    List<Integer> cuentaEventosEntrenador(@Param("id")Integer id);
+    /*
+    SELECT  FK_OrgId, COUNT(DISTINCT FK_UserId)
+    FROM    TableName
+    GROUP   BY FK_OrgId*/
 }
