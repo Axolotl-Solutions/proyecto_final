603591528fee42020d91af97ccca2a0eb77d9dc8
Fernando <fernandondin@ciencias.unam.mx>
2022-06-01 03:14:56 -0500

    Ya calcula el promedio por evento

--- proyecto1/src/main/java/com/unam/proyecto1/repositorio/CalificacionRepositorio.java
+++ proyecto1/src/main/java/com/unam/proyecto1/repositorio/CalificacionRepositorio.java
@@ -4,6 +4,8 @@ import com.unam.proyecto1.modelo.Calificacion;
 import com.unam.proyecto1.modelo.Evento;
 import com.unam.proyecto1.modelo.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -11,4 +13,6 @@ public interface CalificacionRepositorio extends JpaRepository<Calificacion, Int
     boolean existsCalificacionByEventoAndJuezAndCompetidor(Evento evento, Usuario juez, Usuario competidor);
     Calificacion findByEventoAndJuezAndCompetidor(Evento evento, Usuario juez, Usuario competidor);
     List<Calificacion> findByCompetidor(Usuario competidor);
+    @Query(nativeQuery = true, value = "select avg(puntaje) from calificacion where competidor_Id= :idu and evento_Id = :ide group by evento_id;")
+    Integer promedioPorEvento(@Param("idu")Integer idu,@Param("ide")Integer ide);
 }
